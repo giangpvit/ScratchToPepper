1cc33324c344c01f7ccdab4e6adeed304aa158d1
Administrator <>
2017-11-15 18:19:37 +0900

    順番に組み合わせて実行

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -1,3 +1,37 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>終了します</source>
+            <comment>Text</comment>
+            <translation type="obsolete">終了します</translation>
+        </message>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>よくできました</source>
+            <comment>Text</comment>
+            <translation type="unfinished">よくできました</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>もしかして進行方向に障害物がある</source>
+            <comment>Text</comment>
+            <translation type="unfinished">もしかして進行方向に障害物がある</translation>
+        </message>
+    </context>
+</TS>
--- translations/translation_ja_JP.ts
+++ translations/translation_ja_JP.ts
@@ -1,3 +1,32 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ja_JP"/>
+<TS version="2.1" language="ja_JP">
+    <context>
+        <name>behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">こんにちは</translation>
+        </message>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>よくできました</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">こんにちは</translation>
+        </message>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>もしかして進行方向に障害物がある</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
